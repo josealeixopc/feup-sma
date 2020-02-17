30f07786f56295fcd48af9121380d568e350a046
José Aleixo Cruz <josealeixo.pc@gmail.com>
2020-02-17 00:55:54 +0000

    Updaed gitignore

--- spy_vs_spy/plotter.py
+++ spy_vs_spy/plotter.py
@@ -36,6 +36,7 @@ if __name__ == '__main__':
     # plt.show()
     # sys.exit(0)
 
+    # 5 scout files
     keras_dqn_uniqueness_files = [
         "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-13-18-08-30-dqn-keras-uniqueness-red-sniper-env/monitor.csv/openaigym.episode_batch.0.30768.stats.json",
         "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-13-19-14-28-dqn-keras-uniqueness-red-sniper-env/monitor.csv/openaigym.episode_batch.0.32515.stats.json",
@@ -46,6 +47,19 @@ if __name__ == '__main__':
         "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-13-20-18-50-dqn-keras-red-sniper-env/monitor.csv/openaigym.episode_batch.0.3661.stats.json",
         "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-13-21-48-28-dqn-keras-red-sniper-env/monitor.csv/openaigym.episode_batch.0.9769.stats.json"
     ]
+
+    # 20 scout files
+    # keras_dqn_uniqueness_files = [
+    #     "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-14-00-29-38-dqn-keras-uniqueness-red-sniper-env/monitor.csv/openaigym.episode_batch.0.7257.stats.json",
+    #     "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-14-04-46-27-dqn-keras-uniqueness-red-sniper-env/monitor.csv/openaigym.episode_batch.0.10363.stats.json",
+    #     "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-14-11-42-41-dqn-keras-uniqueness-red-sniper-env/monitor.csv/openaigym.episode_batch.0.16056.stats.json"
+    # ]
+    #
+    # keras_dqn_files = [
+    #     "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-14-01-41-46-dqn-keras-red-sniper-env/monitor.csv/openaigym.episode_batch.0.8272.stats.json",
+    #     "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-14-02-53-31-dqn-keras-red-sniper-env/monitor.csv/openaigym.episode_batch.0.9138.stats.json",
+    #     "/home/jazz/Projects/FEUP/ProDEI/feup-sma/spy_vs_spy/trainings/2020-02-14-03-50-13-dqn-keras-red-sniper-env/monitor.csv/openaigym.episode_batch.0.9757.stats.json"
+    # ]
 
     list_rewards_dqn_uniqueness = []
 
@@ -70,10 +84,10 @@ if __name__ == '__main__':
 
     x, y = results_plotter.window_func(np.array(range(len(average_rewards_dqn))),
                                        np.array(average_rewards_dqn),
-                                       100,
+                                       200,
                                        np.mean)
 
-    line, = plt.plot(x, y, linewidth=1, label='dqn')
+    line, = plt.plot(x, y, linewidth=1, label='DQN')
     handles.append(line)
 
     # x, y = smooth_moving_average(range(len(keras_dqn_uniqueness_json['episode_rewards'])),
@@ -82,13 +96,14 @@ if __name__ == '__main__':
 
     x, y = results_plotter.window_func(np.array(range(len(average_rewards_dqn_uniqueness))),
                                        np.array(average_rewards_dqn_uniqueness),
-                                       100,
+                                       200,
                                        np.mean)
 
-    line, = plt.plot(x, y, linewidth=1, label='dqn-uniqueness')
+    line, = plt.plot(x, y, linewidth=1, label='DQN with Coherence Index')
     handles.append(line)
 
     plt.xlim(left=0)
+    plt.title("5 scouts")
     plt.xlabel("Number of Episodes")
     plt.ylabel("Average Reward per Episode")
     plt.tight_layout()
@@ -98,5 +113,5 @@ if __name__ == '__main__':
     xleft, xright = ax.get_xlim()
     ybottom, ytop = ax.get_ylim()
     ax.set_aspect(abs((xright - xleft) / (ybottom - ytop)) * ratio)
-    # plt.savefig("rewards.eps", format='eps', bbox_inches='tight')
+    plt.savefig("rewards_5_scouts.eps", format='eps', bbox_inches='tight')
     plt.show()
